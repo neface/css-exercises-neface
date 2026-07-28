236c7a590fee48e3f1cc5ce8410802d6c6595a11
Necip Çelik <necipfcelik@gmail.com>
2026-07-28 16:11:33 +0300

    intermediate-html-css is finished

---
+++ denemeluk/javacsript.js
@@ -0,0 +1,10 @@
+let pi = 3.14;
+console.log("The value of pi is: " + pi);
+
+const greet = 30;
+
+pi = 3.14159;
+console.log("The updated value of pi is: " + pi);
+console.log("The greeting number is: " + greet);
+greet = 40; // This will cause an error
+console.log("The updated greeting number is: " + greet);
